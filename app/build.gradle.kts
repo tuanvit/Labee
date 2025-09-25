8597b679069bae9d45582098b475e048a9ebd732
LeganSar44 <hoangslevan@gmail.com>
2025-09-26 03:39:26 +0700

    Fix Error Resources APPT in edittext_underline.xml, Resolve Conflict Dependencies for Material Degign in Build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("com.google.android.material:material:1.9.0")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
 }
